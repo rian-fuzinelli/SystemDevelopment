f72d888e459a82b22e6199aa504e753ac21084b4
Rian Fuzinelli <144074554+rian-fuzinelli@users.noreply.github.com>
2024-08-08 19:26:16 -0300

    Create exercicio.cs

---
+++ exercicio.cs
@@ -0,0 +1,44 @@
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        // Título do programa.
+        Console.WriteLine ("Sorteio");
+        
+        // Cria uma instância para o gerador de números.
+        Random numRandom = new Random();
+        
+        // Cria um numero aleatorio entre 0 e 10.
+        int numeroSorteio = numRandom.Next(1, 10);
+        
+        // Variavel para guardar numero do usuario.
+        int numeroUsuario = 0;
+
+        // While cria um loop de 3 rodadas
+        int i = 0;
+        while (i < 3){
+            // Espera um número do usuário.
+            Console.WriteLine ("Digite um numero de 1 a 10");
+            numeroUsuario = int.Parse(Console.ReadLine());
+            
+            // Condição para numeros invalidos.
+            if (numeroUsuario > 10){
+                Console.WriteLine ("Numero invalido");
+                break;
+            }
+            
+            // Condição de acerto.
+            if (numeroUsuario == numeroSorteio){
+                Console.WriteLine ("Voce acertou!");
+                break;
+            }
+            i++;
+        }
+        // Condição de erro.
+        if (numeroUsuario != numeroSorteio && numeroUsuario <= 10){
+            Console.WriteLine ("Voce errou!");
+        }
+    }
+}
